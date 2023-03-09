4341a95facc7b56a2b538b7b62b8a1f696be3288
Adam-Barto <adam.jbarto@gmail.com>
2023-03-09 08:51:38 -0500

    A forest of For loops and if statements cause the test to work.

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -4,23 +4,117 @@ package rocks.zipcodewilmington.tictactoe;
  * @author leon on 6/22/18.
  */
 public class Board {
+    Character[][] game;
+
     public Board(Character[][] matrix) {
+        game = matrix;
     }
 
     public Boolean isInFavorOfX() {
-        return null;
+        return (getCount('X')=='X');
     }
 
     public Boolean isInFavorOfO() {
-        return null;
+        return (getCount('O')=='O');
     }
 
     public Boolean isTie() {
-        return null;
+        return !isInFavorOfX() && !isInFavorOfO();
     }
 
     public String getWinner() {
-        return null;
+        if (isInFavorOfO()){
+            return "O";
+        }else if(isInFavorOfX()){
+            return "X";
+        }else {
+            return "";
+        }
     }
+    public char getCount(char c) {
+        StringBuilder list = new StringBuilder();
+        boolean win = false;
+        for (int y = 0; y < game.length; y++) {
+            if (game[y][y] != c) {
+                win = false;
+                break;
+            } else {
+                win = true;
+            }
+        }
+        if (win) {
+            return c;
+        }
+        for (int y = 0; y < game.length; y++) {
+            if (game[(game.length-1)-y][(game.length-1)-y] != c) {
+                win = false;
+                break;
+            } else {
+                win = true;
+            }
+        }
+        if (win) {
+            return c;
+        }
+        for (int y = 0; y < game.length; y++) {
+            if (game[y][(game.length-1)-y] != c) {
+                win = false;
+                break;
+            } else {
+                win = true;
+            }
+        }
+        if (win) {
+            return c;
+        }
+        for (int y = 0; y < game.length; y++) {
+            if (game[(game.length-1)-y][y] != c) {
+                win = false;
+                break;
+            } else {
+                win = true;
+            }
+        }
+        if (win) {
+            return c;
+        }
 
+        for (int y = 0; y < game.length; y++) {
+            for (int x = 1; x < game.length; x++) {
+                if (game[y][x] == c) {
+                    if (game[y][x] != game[y][x - 1]) {
+                        win = false;
+                        break;
+                    } else {
+                        win = true;
+                    }
+                } else {
+                    win = false;
+                }
+            }
+            if (win) {
+                return c;
+            }
+        }
+
+        for (int y = 0; y < game.length; y++) {
+            for (int x = 1; x < game.length; x++) {
+                if (game[x][y] == c) {
+                    if (game[x][y] != game[x - 1][y]) {
+                        win = false;
+                        break;
+                    } else {
+                        win = true;
+                    }
+                } else {
+                    win = false;
+                }
+            }
+            if (win) {
+                return c;
+            }
+        }
+        return 'p';
+
+    }
 }
